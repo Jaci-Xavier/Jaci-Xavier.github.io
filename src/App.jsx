24a1083005ef968687c91d10b97722a49ee6884c
Jaci-Xavier <devxvr@gmail.com>
2023-10-04 09:18:49 -0300

    adicionando form de contato e iniciando earthcanvas 3d

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from 'react-router-dom'
 
-import { About, Contact, Experience, Feedbacks,
+import { About, Contact, Experience, 
+  // Feedbacks,
   Hero, Navbar, Tech, Works, StarsCanvas } from './components'
 
 const App = () => {
@@ -15,7 +16,7 @@ const App = () => {
         <Experience />
         <Tech />
         <Works />
-        <Feedbacks />
+        {/* <Feedbacks /> */}
         <div className='relative z-0'>
           <Contact />
           <StarsCanvas />
